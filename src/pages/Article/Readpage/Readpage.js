3ca581a4fdc13f49d280d589566a78573ae2afc5
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-01 16:50:51 +0700

    Add design comment component

--- src/pages/Article/Readpage/Readpage.js
+++ src/pages/Article/Readpage/Readpage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Content from './Content/Content'
+import Comment from './Comment/Comment'
 
 const Readpage = () => {
   return (
     <>
         <Content/>
+        <Comment/>
     </>
   )
 }
