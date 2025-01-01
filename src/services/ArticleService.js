b5f327d8e37e9eb5063bc89b70b29af02c4c3f21
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-01 10:16:36 +0700

    Create service getArticleReadpage

--- src/services/ArticleService.js
+++ src/services/ArticleService.js
@@ -40,6 +40,28 @@ class ArticleService {
             throw error;
         }
     };
+
+
+    fetchDataArticleReadpage = async (articleId) => {
+        try {
+            const endpoint = 'http://localhost:8081/api/article/' + articleId;
+            const response = await fetch(
+                endpoint
+            );
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+
+            const data = await response.json();
+
+            if(data.code == 200) {
+                return data.data;
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            throw error;
+        }
+    };
 }
 
 const articleServiceInstance = new ArticleService();
