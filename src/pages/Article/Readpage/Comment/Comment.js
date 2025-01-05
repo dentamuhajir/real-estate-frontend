6deefbb994715d99ae8a2ce502fa77a0d469728a
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-05 14:27:26 +0700

    Update comment components

--- src/pages/Article/Readpage/Comment/Comment.js
+++ src/pages/Article/Readpage/Comment/Comment.js
@@ -3,70 +3,198 @@ import './Comment.css'
 
 const Comment = () => {
   return (
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-5 col-md-6 col-12 pb-4">
-                    <h1>Comments</h1>
-                    <div class="comment mt-4 text-justify float-left">
-                        <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40" />
-                        <h4>Jhon Doe</h4>
-                        <span>- 20 October, 2018</span>
-                        <br/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+    <div class="container mt-5">
+
+            <div class="row  d-flex justify-content-center">
+
+                <div class="col-md-8">
+
+                    <div class="headings d-flex justify-content-between align-items-center mb-3">
+                        <h5>Unread comments(6)</h5>
+
+                        <div class="buttons">
+
+                            <span class="badge-comment bg-white d-flex flex-row align-items-center">
+                                <span class="text-primary">Comments "ON"</span>
+                                <div class="form-check form-switch">
+                                  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+                                  
+                                </div>
+                            </span>
+                            
+                        </div>
+                        
                     </div>
-                    <div class="text-justify darker mt-4 float-right">
-                        <img src="https://i.imgur.com/CFpa3nK.jpg" alt="" class="rounded-circle" width="40" height="40"/>
-                        <h4>Rob Simpson</h4>
-                        <span>- 20 October, 2018</span>
-                        <br/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+
+
+
+                    <div class="card p-3">
+
+                        <div class="d-flex justify-content-between align-items-center">
+
+                      <div class="user d-flex flex-row align-items-center">
+
+                        <img src="https://i.imgur.com/hczKIze.jpg" width="30" class="user-img rounded-circle mr-2" />
+                        <span><small class="font-weight-bold text-primary">james_olesenn</small> <small class="font-weight-bold">Hmm, This poster looks cool</small></span>
+                          
+                      </div>
+                      <small>2 days ago</small>
+                      </div>
+                      <div class="action d-flex justify-content-between mt-2 align-items-center">
+                        <div class="reply px-4">
+                            <small>Remove</small>
+                            <span class="dots"></span>
+                            <small>Reply</small>
+                            <span class="dots"></span>
+                            <small>Translate</small>
+                        </div>
+                        <div class="icons align-items-center">
+                            <i class="fa fa-star text-warning"></i>
+                            <i class="fa fa-check-circle-o check-icon"></i>
+                        </div>
+                      </div>
                     </div>
-                    <div class="comment mt-4 text-justify">
-                        <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40" />
-                        <h4>Jhon Doe</h4>
-                        <span>- 20 October, 2018</span>
-                        <br/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                    <div class="card p-3 mt-2">
+
+                        <div class="d-flex justify-content-between align-items-center">
+
+                      <div class="user d-flex flex-row align-items-center">
+
+                        <img src="https://i.imgur.com/C4egmYM.jpg" width="30" class="user-img rounded-circle mr-2" />
+                        <span><small class="font-weight-bold text-primary">olan_sams</small> <small class="font-weight-bold">Loving your work and profile! </small></span>
+                          
+                      </div>
+
+
+                      <small>3 days ago</small>
+
+                      </div>
+
+
+                      <div class="action d-flex justify-content-between mt-2 align-items-center">
+
+                        <div class="reply px-4">
+                            <small>Remove</small>
+                            <span class="dots"></span>
+                            <small>Reply</small>
+                            <span class="dots"></span>
+                            <small>Translate</small>
+                           
+                        </div>
+
+                        <div class="icons align-items-center">
+                            <i class="fa fa-check-circle-o check-icon text-primary"></i>
+                            
+                        </div>
+                          
+                      </div>
+
+
+                        
                     </div>
-                    <div class="darker mt-4 text-justify">
-                        <img src="https://i.imgur.com/CFpa3nK.jpg" alt="" class="rounded-circle" width="40" height="40"/>
-                        <h4>Rob Simpson</h4>
-                        <span>- 20 October, 2018</span>
-                        <br/>
-                        <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+
+
+
+
+
+
+
+
+
+
+                    <div class="card p-3 mt-2">
+
+                        <div class="d-flex justify-content-between align-items-center">
+
+                      <div class="user d-flex flex-row align-items-center">
+
+                        <img src="https://i.imgur.com/0LKZQYM.jpg" width="30" class="user-img rounded-circle mr-2" />
+                        <span><small class="font-weight-bold text-primary">rashida_jones</small> <small class="font-weight-bold">Really cool Which filter are you using? </small></span>
+                          
+                      </div>
+
+
+                      <small>3 days ago</small>
+
+                      </div>
+
+
+                      <div class="action d-flex justify-content-between mt-2 align-items-center">
+
+                        <div class="reply px-4">
+                            <small>Remove</small>
+                            <span class="dots"></span>
+                            <small>Reply</small>
+                            <span class="dots"></span>
+                            <small>Translate</small>
+                           
+                        </div>
+
+                        <div class="icons align-items-center">
+                            <i class="fa fa-user-plus text-muted"></i>
+                            <i class="fa fa-star-o text-muted"></i>
+                            <i class="fa fa-check-circle-o check-icon text-primary"></i>
+                            
+                        </div>
+                          
+                      </div>
+
+
+                        
                     </div>
+
+
+
+
+
+
+                    <div class="card p-3 mt-2">
+
+                        <div class="d-flex justify-content-between align-items-center">
+
+                      <div class="user d-flex flex-row align-items-center">
+
+                        <img src="https://i.imgur.com/ZSkeqnd.jpg" width="30" class="user-img rounded-circle mr-2" />
+                        <span><small class="font-weight-bold text-primary">simona_rnasi</small> <small class="font-weight-bold text-primary">@macky_lones</small> <small class="font-weight-bold text-primary">@rashida_jones</small> <small class="font-weight-bold">Thanks </small></span>
+                          
+                      </div>
+
+
+                      <small>3 days ago</small>
+
+                      </div>
+
+
+                      <div class="action d-flex justify-content-between mt-2 align-items-center">
+
+                        <div class="reply px-4">
+                            <small>Remove</small>
+                            <span class="dots"></span>
+                            <small>Reply</small>
+                            <span class="dots"></span>
+                            <small>Translate</small>
+                           
+                        </div>
+
+                        <div class="icons align-items-center">
+                           
+                            <i class="fa fa-check-circle-o check-icon text-primary"></i>
+                            
+                        </div>
+                          
+                      </div>
+
+
+                        
+                    </div>
+
+
+                    
                 </div>
-                <div class="col-lg-4 col-md-5 col-sm-4 offset-md-1 offset-sm-1 col-12 mt-4">
-                    <form id="algin-form">
-                        <div class="form-group">
-                            <h4>Leave a comment</h4>
-                            <label for="message">Message</label>
-                            <textarea name="msg" id=""msg cols="30" rows="5" class="form-control" style={{ backgroundColor: 'black' }}></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" id="fullname" class="form-control"/>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" id="email" class="form-control"/>
-                        </div>
-                        <div class="form-group">
-                            <p class="text-secondary">If you have a <a href="#" class="alert-link">gravatar account</a> your address will be used to display your profile picture.</p>
-                        </div>
-                        <div class="form-inline">
-                            <input type="checkbox" name="check" id="checkbx" class="mr-1"/>
-                            <label for="subscribe">Subscribe me to the newlettter</label>
-                        </div>
-                        <div class="form-group">
-                            <button type="button" id="post" class="btn">Post Comment</button>
-                        </div>
-                    </form>
-                </div>
+                
             </div>
+            
         </div>
-    </section>
   )
 }
 
