5d4bb284cc302b17126adda8a6823eb3f536ea09
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-06 11:25:24 +0700

    Add function handleSubmit form comment

--- src/pages/Article/Readpage/Comment/PostComment/PostComment.js
+++ src/pages/Article/Readpage/Comment/PostComment/PostComment.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const PostComment = () => {
-  const handleSubmit = (e) => {
-    alert("form submitted")
+
+  const { id } = useParams();
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const inputValue = formData.get('inputComment');
+    
+    console.log(inputValue, id)
   }
-  
+
   return (
     <div class="col-md-8">
       <form onSubmit={handleSubmit} >
         <div class="d-flex flex-start w-100">
             <img class="rounded-circle shadow-1-strong me-3" src="https://i.pravatar.cc/50" alt="avatar" width="40" height="40" />
             <div data-mdb-input-init class="form-outline w-100">
-                <textarea class="form-control" id="textAreaExample" rows="4" placeholder="Write your comment"></textarea>
+                <textarea class="form-control" name="inputComment" rows="4" placeholder="Write your comment" ></textarea>
             </div>
         </div>
         <div class="float-end mt-2 pt-1">
