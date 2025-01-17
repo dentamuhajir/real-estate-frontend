3cf2bf613f8c513cab0d201b83b3a691ce6f0612
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-17 23:07:51 +0700

    Update eslint

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -10,6 +10,11 @@ import Footer from "./Footer/Footer"
 import IndexArticle from "../pages/Article/Index/Index";
 import ReadpageArticle from "../pages/Article/Readpage/Readpage";
 import Griya from "../pages/Griya/Griya";
+import Application from "../pages/Application/Application";
+<<<<<<< Updated upstream
+=======
+import Property from "../pages/Property/Property";
+>>>>>>> Stashed changes
 
 function Main() {
     return (
@@ -20,6 +25,11 @@ function Main() {
                         <Route path="/" element={<Griya/>} />
                         <Route path="/article" element={<IndexArticle/>} />
                         <Route path="/article/readpage/:id/:slug" element={<ReadpageArticle/>} />
+                        <Route path="/application" element={<Application/>} />
+<<<<<<< Updated upstream
+=======
+                        <Route path="/propery" element={<Property/>} />
+>>>>>>> Stashed changes
                     </Routes>
                 </div>
             <Footer/>
