ad05b8bf9d4843983b272127bcbd8b6f4451152f
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-22 00:26:58 +0700

    Add placeholder loading

--- src/pages/Article/Index/Headline/Headline.js
+++ src/pages/Article/Index/Headline/Headline.js
@@ -24,24 +24,53 @@ const Headline = () => {
     return (
         <div class="container mt-5">
             <div class="row">
-                { article.map((item) => {
-                    return (
-                        <div class="col-md-6">
-                            <NavLink to={"/article/readpage/" + item.id + "/" + item.title.replace(/[\W_]/g, '-').toLowerCase() } >
-                                <div class="card bg-dark text-white border-0 rounded-4">
-                                    <img class="card-img image-headline rounded-4"
-                                        src={ item.photo }
-                                        alt="item.imageAlt" />
-                                    <div class="card-img-overlay rounded-4">
-                                        <h5 class="card-title">{ item.title } </h5>
-                                        <p class="card-text"></p>
-                                        <p class="card-text">{ item.category }  </p>
-                                    </div>
+                { article.length > 0 ?
+                    (
+                        article.map((item) => {
+                            return (
+                                <div class="col-md-6">
+                                    <NavLink to={"/article/readpage/" + item.id + "/" + item.title.replace(/[\W_]/g, '-').toLowerCase() } >
+                                        <div class="card bg-dark text-white border-0 rounded-4">
+                                            <img class="card-img image-headline rounded-4"
+                                                src={ item.photo }
+                                                alt="item.imageAlt" />
+                                            <div class="card-img-overlay rounded-4">
+                                                <h5 class="card-title">{ item.title } </h5>
+                                                <p class="card-text"></p>
+                                                <p class="card-text">{ item.category }  </p>
+                                            </div>
+                                        </div>
+                                    </NavLink>
                                 </div>
-                            </NavLink>
+                            )                    
+                        })
+                    )
+                    : 
+                    (
+                        <>
+                        <div class="col-md-6 placeholder-wave">
+                            <div class="card bg-dark text-white border-0 rounded-4 placeholder bg-light">
+                                <img class="card-img image-headline image-headline-placeholder rounded-4 placeholder" alt="item.imageAlt" />
+                                <div class="card-img-overlay card-img-overlay-headline rounded-4">
+                                    <h5 class="card-title placeholder bg-light"></h5>
+                                    <p class="card-text placeholder bg-light"></p>
+                                    <p class="card-text placeholder bg-light"></p>
+                                </div>
+                            </div>
                         </div>
-                    )                    
-                })}
+                        <div class="col-md-6 placeholder-wave">
+                            <div class="card bg-dark text-white border-0 rounded-4 placeholder bg-light">
+                                <img class="card-img image-headline image-headline-placeholder rounded-4 placeholder" alt="item.imageAlt"/>
+                                <div class="card-img-overlay card-img-overlay-headline rounded-4">
+                                    <h5 class="card-title placeholder bg-light"></h5>
+                                    <p class="card-text placeholder bg-light"></p>
+                                    <p class="card-text placeholder bg-light"></p>
+                                </div>
+                            </div>
+                        </div>
+                        </>
+                    )
+                }
             </div>
         </div>
   )
