e38442bfdc0698bc161350f217e45756f5a21913
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-23 21:39:19 +0700

    Create component Buy Ability form

--- src/pages/BuyAbility/Form/Form.js
+++ src/pages/BuyAbility/Form/Form.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <form>
+      <div class="mb-3">
+        <label for="" className="form-label">How much have you saved for a down payment?</label>
+        <input type="text" className="form-control" id="" />
+      </div>
+
+      <div class="mb-3">
+        <label for="" className="form-label">How much are you comfortable spending per month?</label>
+        <input type="text" className="form-control" id="" />
+        <div id="" className="form-text">We’ll try to keep the monthly payment we calculate within your budget.</div>
+      </div>
+
+      <div class="d-grid gap-2">
+        <button type="submit" className="btn btn-primary btn-large">See what you can afford</button>
+      </div>
+    </form>
   )
 }
 
