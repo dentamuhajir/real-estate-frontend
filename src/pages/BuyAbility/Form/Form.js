60f6485c0616ae85a3234ed4af92401fd81edfe0
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-25 15:11:53 +0700

    Update handle form submit on feature Buy Ability

--- src/pages/BuyAbility/Form/Form.js
+++ src/pages/BuyAbility/Form/Form.js
@@ -1,34 +1,46 @@
 import React from 'react'
 
 const Form = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    let propertyPrice = formData.get('property-price')
+    let downPayment = formData.get('down-payment')
+    let monthlyIncome = formData.get('monthly-income')
+    let loanTenure = formData.get('loan-tenure')
+    let annualInterestRate = formData.get('annual-interest-rate')
+
+    
+    alert("click " + formData.get('property-price'));
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div class="mb-3">
         <label for="" className="form-label">What is the price of the property you want to buy??</label>
-        <input type="text" className="form-control" id="" />
+        <input type="text" name='property-price' className="form-control" id="" />
       </div>
 
       <div class="mb-3">
         <label for="" className="form-label">How much do you have for the down payment?</label>
-        <input type="text" className="form-control" id="" />
+        <input type="text" name='down-payment' className="form-control" id="" />
         <div id="" className="form-text">We’ll try to keep the monthly payment we calculate within your budget.</div>
       </div>
 
       <div class="mb-3">
         <label for="" className="form-label">What is your monthly income</label>
-        <input type="text" className="form-control" id="" />
+        <input type="text" name="monthly-income" className="form-control" id="" />
         <div id="" className="form-text">Enter your total income before taxes or deductions.</div>
       </div>
       
       <div class="mb-3">
         <label for="" className="form-label">How long do you want to pay off the loan?</label>
-        <input type="text" className="form-control" id="" />
+        <input type="text" name="loan-tenure" className="form-control" id="" />
         <div id="" className="form-text">Enter the loan tenure in years (e.g., 5, 10, 15, or 20 years).</div>
       </div>
 
       <div class="mb-3">
         <label for="" className="form-label">What is the annual interest rate offered by the bank?</label>
-        <input type="text" className="form-control" id="" />
+        <input type="text" name="annual-interest-rate" className="form-control" id="" />
         <div id="" className="form-text">Enter the loan's annual interest rate in percentage.</div>
       </div>
 
