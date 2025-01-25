72b3c88c63442382250d92d2bc923ca6a74c67a8
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-25 12:40:04 +0700

    Recommit update form

--- src/pages/BuyAbility/BuyAbility.js
+++ src/pages/BuyAbility/BuyAbility.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Form from './Form/Form'
 
 const BuyAbility = () => {
   return (
-    <div>BuyAbility</div>
+      <div className="container-fluid w-50 mt-3">
+        <h3 className='text-center'>BuyAbility&trade;</h3>
+        <p className='text-center fw-bold'>Buying a home is a big move. We're here to help you every step of the way.</p>
+        <p className='text-center'>Let’s figure out what you can afford and track how it changes over time. If you’re not sure of something, it’s okay to take your best guess</p>
+      <Form/>
+      </div>
   )
 }
 
--- src/pages/BuyAbility/Form/Form.js
+++ src/pages/BuyAbility/Form/Form.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <form>
+      <div class="mb-3">
+        <label for="" className="form-label">What is the price of the property you want to buy??</label>
+        <input type="text" className="form-control" id="" />
+      </div>
+
+      <div class="mb-3">
+        <label for="" className="form-label">How much do you have for the down payment?</label>
+        <input type="text" className="form-control" id="" />
+        <div id="" className="form-text">We’ll try to keep the monthly payment we calculate within your budget.</div>
+      </div>
+
+      <div class="mb-3">
+        <label for="" className="form-label">What is your monthly income</label>
+        <input type="text" className="form-control" id="" />
+        <div id="" className="form-text">Enter your total income before taxes or deductions.</div>
+      </div>
+      
+      <div class="mb-3">
+        <label for="" className="form-label">How long do you want to pay off the loan?</label>
+        <input type="text" className="form-control" id="" />
+        <div id="" className="form-text">Enter the loan tenure in years (e.g., 5, 10, 15, or 20 years).</div>
+      </div>
+
+      <div class="mb-3">
+        <label for="" className="form-label">What is the annual interest rate offered by the bank?</label>
+        <input type="text" className="form-control" id="" />
+        <div id="" className="form-text">Enter the loan's annual interest rate in percentage.</div>
+      </div>
+
+      <div class="d-grid gap-2">
+        <button type="submit" className="btn btn-primary btn-large">See what you can afford</button>
+      </div>
+    </form>
   )
 }
 
