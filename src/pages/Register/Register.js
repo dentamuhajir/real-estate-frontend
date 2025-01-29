52d1c68251176db6297c51ad0c3f2a1ef97fc4a0
Denta Muhajir <dentamuhajir@gmail.com>
2025-01-29 11:10:24 +0700

    Handle submit register form

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,6 +3,18 @@ import React from 'react'
 const Register = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
+        const formData = new FormData(event.target)
+ 
+        const formValues = [{
+            fullName: formData.get('full-name'),
+            email: formData.get('email'),
+            address: formData.get('address'),
+            //idCard: formData.get('id-card'),
+            password: formData.get('password'),
+            confirmPassword: formData.get('confirm-password')  
+        }]
+
+        console.log(formValues)
     }
 
     return (
@@ -11,28 +23,28 @@ const Register = () => {
             <p className='text-center fw-bold'>Buying a home is a big move. We're here to help you every step of the way.</p>
             <form onSubmit={handleSubmit}>
             <div class="mb-3">
-                <label for="" className="form-label">Name</label>
-                <input type="text" name='name' className="form-control" id="" required/>
+                <label for="" className="form-label">Full Name</label>
+                <input type="text" name="full-name" className="form-control" id="" required/>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email Address</label>
-                <input type="email" class="form-control" id="email" required />
+                <input type="email" name="email" class="form-control" id="email" required />
             </div>
             <div class="mb-3">
                 <label for="address" class="form-label">Address</label>
-                <textarea class="form-control" id="address" rows="2" required></textarea>
+                <textarea name="address" class="form-control" id="address" rows="2" required></textarea>
             </div>
             <div class="mb-3">
                 <label for="idCard" class="form-label">Upload ID Card</label>
-                <input type="file" class="form-control" id="idCard" accept="image/*,.pdf" required />
+                <input type="file" name="id-card" class="form-control" id="idCard" accept="image/*,.pdf"  />
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" required />
+                <input type="password" name="password" class="form-control" id="password" required />
             </div>
             <div class="mb-3">
                 <label for="confirmPassword" class="form-label">Retype Password</label>
-                <input type="password" class="form-control" id="confirmPassword" required />
+                <input type="password" name="confirm-password" class="form-control" id="confirmPassword" required />
             </div>
             <div class="d-grid gap-2">
                 <button type="submit" className="btn btn-primary btn-large">Register</button>
